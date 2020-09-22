32e5c58934f0f2cc27a2f5ad26ba2b8827f7f5c6
RaginiKumari <58027796+melodyni@users.noreply.github.com>
2020-09-22 18:25:27 +0530

    added handleLogin

--- handlers.js
+++ handlers.js
@@ -1,4 +1,25 @@
 const request = require('superagent');
+const database = require('./data.json');
+
+const registerNewUser = function (req, res) {
+  const { id, avatar } = req.session;
+  const userInfo = { id, avatar, ...req.body };
+  console.log({ userInfo, artWork: [] });
+  database.push({ userInfo, artWork: [] });
+  res.redirect('/gallery');
+};
+
+const handleLogin = function (req, res) {
+  const { sub, name, picture } = req.userInfo;
+  const user = {
+    id: sub,
+    name,
+    avatar: picture,
+    sessionCreatedAt: new Date().toJSON(),
+  };
+  req.session = user;
+  res.redirect(req.app.locals.ReactServer);
+};
 
 const getToken = (code, credentials) => {
   const { ClientId, ClientSecret, RedirectUri } = credentials;
@@ -18,20 +39,27 @@ const getToken = (code, credentials) => {
 const getUserInfo = function ({ token_type, access_token }) {
   return request
     .get(`https://www.googleapis.com/oauth2/v3/userinfo`)
-    .set('User-Agent', 'googleauthlogin')
+    .set('User-Agent', 'artInAction')
     .set('Authorization', `${token_type} ${access_token}`)
     .then((res) => res.body);
 };
 
-const fetchDetails = async (req, res) => {
+const fetchDetails = async (req, res, next) => {
   const code = req.query.code;
   try {
     const token = await getToken(code, req.app.locals);
-    const userInfo = await getUserInfo(token);
-    res.end();
+    req.userInfo = await getUserInfo(token);
+    next();
   } catch (err) {
+    console.log('err', err);
     return res.send(err);
   }
 };
 
-module.exports = { fetchDetails };
+const serveAuthUrl = (req, res) => {
+  const url =
+    'https://accounts.google.com/o/oauth2/v2/auth?client_id=255108424820-8e1p39r9u251c8gagoklt9nb1e8vngoq.apps.googleusercontent.com&response_type=code&redirect_uri=http://localhost:3002/auth/user&scope=https%3A//www.googleapis.com/auth/userinfo.profile';
+  res.json({ url });
+};
+
+module.exports = { fetchDetails, handleLogin, registerNewUser, serveAuthUrl };
--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const PORT = 3002;
 const express = require('express');
+const session = require('cookie-session');
 const morgan = require('morgan');
 const app = express();
 
-const { fetchDetails } = require('./handlers');
-const { ClientId, ClientSecret, RedirectUri } = require('./config.js');
-app.locals = { ClientId, ClientSecret, RedirectUri };
+const {
+  fetchDetails,
+  handleLogin,
+  registerNewUser,
+  serveAuthUrl,
+} = require('./handlers');
+
+const {
+  ClientId,
+  ClientSecret,
+  RedirectUri,
+  ReactServer,
+  CookieSecret,
+} = require('./config.js');
+
+app.locals = { ClientId, ClientSecret, RedirectUri, ReactServer };
 
 app.use(morgan('tiny'));
 app.use(express.json());
+app.set('sessionMiddleware', session({ secret: CookieSecret }));
+app.use((...args) => app.get('sessionMiddleware')(...args));
 
-app.get('/auth/user', fetchDetails);
+app.post('/api/register', registerNewUser);
+
+app.get('/auth/user', fetchDetails, handleLogin);
+app.get('/auth/init', serveAuthUrl);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
